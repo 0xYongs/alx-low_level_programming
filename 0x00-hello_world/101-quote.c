bd3f78dfdde91ad0424605567262f2a4ab01d9ca
tradingsensei <tradingsensei21@gmail.com>
2023-03-16 06:38:18 -0700

    exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ *
+ * Main - Entry point
+ *
+ * Description: print out text without puts or printf
+ *
+ * Return: Error 1
+ */
+
+int main (void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
