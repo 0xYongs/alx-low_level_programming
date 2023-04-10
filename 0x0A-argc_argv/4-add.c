b5cf10e556acce17e770bd2d4f8b037c48618110
tradingsensei <tradingsensei21@gmail.com>
2023-04-10 05:09:18 -0700

    Add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - program that adds positive numbers
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 1 on ERROR and 0 on Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("ERROR\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
