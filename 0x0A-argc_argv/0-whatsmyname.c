619227debb3a4037126cb81e11d8a2681b740f8b
tradingsensei <tradingsensei21@gmail.com>
2023-04-10 03:38:49 -0700

    Print name of this program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - program that prints its name, followed by a new line
+ *
+ * @argc: int type parameter
+ * @argv: pointer to char type parameter
+ *
+ * Return: 0 on success
+ */
+
+int main(int __attribute__((unused))argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
