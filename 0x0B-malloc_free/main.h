359e304182dd3b99abd95184a5c6dd7bcf245449
tradingsensei <tradingsensei21@gmail.com>
2023-04-11 04:17:07 -0700

    Free a 2 dimensional grid previously created by your alloc_grid function

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,3 +8,4 @@ char *strcpy(char *dest, const char *src);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
