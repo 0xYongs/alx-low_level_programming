eda19148f606b591d3c3f1c465e89c210b0475a5
tradingsensei <tradingsensei21@gmail.com>
2023-04-17 09:22:40 -0700

    Free dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,11 @@ typedef struct dog
 /**
  * dog_t - typedef for struct dog
  * @d: pointer to struct
+ * @name: 1st member
+ * @age: dog's age
+ * @owner: owner of dog
  */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
