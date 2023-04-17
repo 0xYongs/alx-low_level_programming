75f1605c8c6990496dd8fbc2e440b006849f7b93
tradingsensei <tradingsensei21@gmail.com>
2023-04-17 05:54:52 -0700

    Define a new type struct dog with three elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - Define a new type struct dog with the following elements
  *
@@ -8,7 +11,9 @@
 
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
+
+#endif /*dog.h*/
