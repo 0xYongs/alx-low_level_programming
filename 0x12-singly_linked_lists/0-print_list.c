a15b0c06f296fbec58c203cfe7965e3cec3a9295
tradingsensei <tradingsensei21@gmail.com>
2023-04-28 03:56:35 -0700

    Return the number of elements in a linked list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,11 +11,15 @@ size_t print_list(const list_t *h)
 {
 	size_t node;
 
+	if (h == NULL)
+		return (0);
+
 	for (node = 0; h; node++)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-		printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
 	return (node);
